dfd25eed7934caa20424e2bfba99742d274cccf8
Mohammad <Mohammadhusen257@gmail.com>
2019-05-08 20:46:34 +0545

    project analysis added

--- Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/controller/HomeController.java
+++ Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.mhs.goodsexchangehinge;
+package com.mhs.goodsexchangehinge.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
---
+++ Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/model/User.java
@@ -0,0 +1,8 @@
+package com.mhs.goodsexchangehinge.model;
+
+public class User {
+	private int uid;
+	private String fullName;
+	private String address;
+	private String phone;
+}
