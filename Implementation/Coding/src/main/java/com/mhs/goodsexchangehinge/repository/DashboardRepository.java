5b3450c28b952d74a70b0696f2a8191ec8d321de
Mohammad <Mohammadhusen257@gmail.com>
2019-07-01 16:51:34 +0545

    dashboard
    Count total to display some info in dashboard

--- Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/repository/DashboardRepository.java
+++ Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/repository/DashboardRepository.java
@@ -1,7 +1,22 @@
 package com.mhs.goodsexchangehinge.repository;
 
+import java.util.List;
+
+import com.mhs.goodsexchangehinge.model.Category;
+import com.mhs.goodsexchangehinge.model.Faq;
+import com.mhs.goodsexchangehinge.model.ProductExchange;
+import com.mhs.goodsexchangehinge.model.ProductRequest;
+import com.mhs.goodsexchangehinge.model.User;
+
 public interface DashboardRepository {
 
-	public void countProductExchangeRow();
+	public List<ProductExchange> countAllExchangeProduct();
 
+	public List<ProductRequest> countAllRequestProduct();
+
+	public List<User> countAllUser();
+
+	public List<Faq> countAllFaq();
+
+	public List<Category> countAllCategory();
 }
--- Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/service/DashboardService.java
+++ Implementation/Coding/src/main/java/com/mhs/goodsexchangehinge/service/DashboardService.java
@@ -1,5 +1,23 @@
 package com.mhs.goodsexchangehinge.service;
 
+import java.util.List;
+
+import com.mhs.goodsexchangehinge.model.Category;
+import com.mhs.goodsexchangehinge.model.Faq;
+import com.mhs.goodsexchangehinge.model.ProductExchange;
+import com.mhs.goodsexchangehinge.model.ProductRequest;
+import com.mhs.goodsexchangehinge.model.User;
+
 public interface DashboardService {
-	public void countProductExchangeRow();
+
+	public List<ProductExchange> countAllExchangeProduct();
+
+	public List<ProductRequest> countAllRequestProduct();
+
+	public List<User> countAllUser();
+
+	public List<Faq> countAllFaq();
+
+	public List<Category> countAllCategory();
+
 }
